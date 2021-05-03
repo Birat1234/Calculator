59673f823efee7d89909a900580287a437007498
Birat Dhar <birat.dhar.89@gmail.com>
2021-05-03 11:31:07 +0530

    1st commit

---
+++ script.js
@@ -0,0 +1,38 @@
+let screen = document.getElementById('screen');
+
+buttons = document.querySelectorAll('button');
+
+let screenvalue = '';
+let screenvalue1 = '';
+let screenvalue2 = '';
+
+for(item of buttons){
+
+    item.addEventListener('click', (e)=>{
+        button_text = e.target.innerText;
+
+        console.log(button_text);
+
+        if(button_text == 'X')
+        {
+            button_text = '*';
+            screenvalue += button_text;
+            screen.value = screenvalue;
+        }
+        else if (button_text == 'C')
+        {
+            screenvalue = '';
+            screen.value = screenvalue;
+        }
+        else if (button_text == '=')
+        {
+            screen.value = eval(screenvalue);
+        }
+        
+        else{
+            screenvalue += button_text;
+            screen.value = screenvalue;
+        }
+    })
+
+}
